9b4ea34c3dee7af64b02f190984f2963037af1fb
relarizky <relarizky@gmail.com>
2022-01-30 10:55:51 +0700

    finished config_file_reader func and its test

--- library/utility.py
+++ library/utility.py
@@ -1,3 +1,36 @@
-def api_key_reader():
+import os
+import json
+
+from typing import Union
+
+
+def config_file_reader() -> dict:
+    """CONFIG FILE READER
+
+    function for reading config json file.
+
+    Parameters
+        None
+
+    Returns
+        content_json (dict): dictionary as result of parsed config nested json
+    """
+
+    FILE = os.getcwd() + "/.config.json"
+    content_json = {}
+
+    if os.path.isfile(FILE):
+        with open(FILE, "r") as file:
+            content = file.read()
+            content_json = json.loads(content)
+
+    return content_json
+
+
+def result_saver(site_list: list, domain: str) -> None:
+    """OBTAINED RESULT SAVER
+
+    function for saving the obtained result into save/ directory
+    """
 
     pass
--- tests/unit/test_utility.py
+++ tests/unit/test_utility.py
@@ -1,5 +1,37 @@
-from library.utility import api_key_reader
+import pytest
+import builtins
 
-def test_api_key_reader(monkeypatch):
+from library.utility import config_file_reader
 
-    pass
+
+class MockTextIOWrapper:
+    """Class For Mocking Out TextIOWrapper"""
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        pass
+
+    def read(self):
+        return "{\"text\": \"Hello World\"}"
+
+
+@pytest.mark.utility
+def test_config_file_reader(monkeypatch):
+
+    def mock_open(*args, **kwargs):
+        return MockTextIOWrapper()
+
+    # mock out the open builtin func to avoid real file reading
+    with monkeypatch.context() as mock:
+        mock.setattr(builtins, "open", mock_open)
+        api_key = config_file_reader()
+
+    assert api_key.get("text") == "Hello World"
+
+
+@pytest.mark.utility
+def test_result_saver(monkeypatch):
+
+    assert True
