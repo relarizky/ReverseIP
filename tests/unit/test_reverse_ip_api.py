8f9c4ca0885a7639bd727b818690a2fff38b9902
relarizky <relarizky@gmail.com>
2022-01-31 14:30:16 +0700

    added test_api_hacker_target

--- tests/unit/test_reverse_ip_api.py
+++ tests/unit/test_reverse_ip_api.py
@@ -4,17 +4,13 @@ import requests
 from library.api.whois_xml_api import WhoisXMLAPI
 from library.api.hacker_target_api import HackerTargetAPI
 
-# i use parametrize in case you want to add more API classes that use
-# the same GET or POST method for communcating with API endpoint
-
 @pytest.mark.api
-@pytest.mark.parametrize("api_obj_ref", [HackerTargetAPI])
-def test_api_get_request(monkeypatch, mock_response, api_obj_ref):
+def test_api_hacker_target(monkeypatch, mock_response):
 
     # mock text attribute of get class
     monkeypatch.setattr(requests, "get", mock_response)
 
-    obj = api_obj_ref("target.com")
+    obj = HackerTargetAPI("target.com")
     obj.fetch()
     obj.scrap()
 
